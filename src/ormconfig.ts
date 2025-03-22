788fd03e1c77e6b68aef7df0770314866830b803
Alexandr <smolnikov.456@mail.ru>
2025-03-22 12:14:08 +0300

    feat: add migration for create users table, update orm typeorm config

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,17 +1,17 @@
 import * as dotenv from 'dotenv';
-import { DataSource } from 'typeorm';
 import * as path from 'node:path';
+import { DataSource } from 'typeorm';
 
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
 export default new DataSource({
   type: 'postgres',
   host: process.env.PG_HOST,
-  port: parseInt(process.env.PG_PORT!, 10),
+  port: Number(process.env.PG_PORT),
   username: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE_NAME,
-  entities: ['/**/*.entity{.ts}'],
-  migrations: ['migrations/*{.ts}'],
+  entities: [path.join(__dirname, '**/**/*.entity{.ts,.js}')],
+  migrations: [path.join(__dirname, 'migrations/*{.ts,.js}')],
   synchronize: false,
 });
